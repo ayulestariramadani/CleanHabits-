3bb984f616b6699b02d9632c858775f4adaa3f6c
ayulestariramadani <ayulestariramadani@gmail.com>
2023-05-24 10:00:11 +0700

    Initial Commit

---
+++ CleanHabits/CleanHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanHabitsApp.swift
+//  CleanHabits
+//
+//  Created by Ayu Lestari Ramadani on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CleanHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanHabits/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CleanHabits
+//
+//  Created by Ayu Lestari Ramadani on 24/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
